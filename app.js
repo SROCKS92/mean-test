8bb0e7b86fa37237b648f48f5f453498c4e33e60
Sarang Wadekar <sarang@Sarangs-MacBook-Air.local>
2017-05-28 21:21:55 +0530

    starting app

---
+++ app.js
@@ -0,0 +1,22 @@
+var app=angular.module('userTask',['userController','userService','UserprofileService','login','ngRoute'])
+.config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'js/partial/login.html',
+        controller: 'loginController'
+      })
+      .when('/register', {
+        templateUrl: 'js/partial/register.html',
+        controller: 'mainController'
+      })
+       .when('/todo', {
+        templateUrl: 'js/partial/todo.html',
+        controller: 'mainController'
+      })
+        .when('/dashboard/:id', {
+        templateUrl: 'js/partial/dashboard.html',
+        controller: 'mainController'
+      }).otherwise({
+        redirectTo: '/login'
+      });
+  });
